e5e13eb266901abdb136e639d37b8d68a67c6505
TenmaMune <tenmachinen@gmail.com>
2022-08-27 13:37:58 +0200

    Debugging Mode Removed

--- works/js/card.js
+++ works/js/card.js
@@ -1,10 +1,10 @@
 function createCard(work) {
-  // <a class="work-card-wrapper" href=${''}# target="_blank"></a>
   const imgPath = work.img || 'works/assets/images/sample.png';
-  const icons = work.groups.reduce( (acc,group) =>{
+  const icons = work.groups.reduce((acc, group) => {
     return acc + `<span class="group group-${group.toLowerCase()}">${group}</span>`
-  },'');
-  console.log(icons);
+  }, '');
+  const links = work.links;
+
   return `
     <div class="work-card">
       <div class="work-card-head">
@@ -16,8 +16,8 @@ function createCard(work) {
       <div class="work-card-foot">
         <div class="group-box">${icons}</div>
         <div class="links-box">
-         <a href="${work.links.git}" target="_blank"><div class="icon icon-github"></div></a>
-          <a href="${work.links.web}" target="_blank"><div class="icon icon-website"></div></a>
+         <a ${links.git ? 'href=' : ''}"${links.git}" target="_blank"><div class="icon icon-github ${links.git?'':'disabled'}"></div></a>
+         <a ${links.web ? 'href=' : ''}"${links.web}" target="_blank"><div class="icon icon-website ${links.web?'':'disabled'}"></div></a>
         </div>
       </div>
     </a>
--- works/js/data.js
+++ works/js/data.js
@@ -1,10 +1,12 @@
+const BASE_DIR = '/works/assets/images/';
+
 const worksInfo = [
   {
     "groups": ['Python'],
     "title": "PySnip",
     "desc": "Desktop snipper tool enhancer",
     "date": { "start": "22/06/10", "end": "22/06/20" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -20,7 +22,7 @@ const worksInfo = [
     "title": "Python Desktop Color Notes",
     "desc": "Create grouped customizable notes  to improve organization for daily tasks or ideas.",
     "date": { "start": "20/10", "end": "20/11" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -28,7 +30,7 @@ const worksInfo = [
     "title": "ST3 Autoformat Plugin",
     "desc": "Automatic format Python code to a morea readable style by a given shortcut.( Not PEP8 standards )",
     "date": { "start": "20/09", "end": "20/10" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -36,7 +38,7 @@ const worksInfo = [
     "title": "OpenCV Tools Desktop App",
     "desc": "Simple toolkit to apply filters and several detection algorithms as contours, lines and much more.",
     "date": { "start": "22/04", "end": "20/05" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -44,7 +46,7 @@ const worksInfo = [
     "title": "Binance Arbitrage Finder",
     "desc": "Gathering data from Binance stores and notifies each opportunity in an Android App",
     "date": { "start": "20/09", "end": "20/10" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
 
@@ -53,7 +55,7 @@ const worksInfo = [
     "title": "Whatsapp Artists Stickers App",
     "desc": "Artists friendly app. Show and organize stickers stored in Google Drive. Customizable whatsapp sticker packages.",
     "date": { "start": "20/09", "end": "20/10" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -61,7 +63,7 @@ const worksInfo = [
     "title": "Color Notes App",
     "desc": "Simple and intuitive TODO tasks app using colored notes in Android.",
     "date": { "start": "20/09", "end": "20/10" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -69,7 +71,7 @@ const worksInfo = [
     "title": "VStack game",
     "desc": "Small arcade game, styled as old arcade games, whose goal is to stack blocks on top of each other until reach the top. With several levels.",
     "date": { "start": "20/09", "end": "20/10" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -77,8 +79,8 @@ const worksInfo = [
     "title": "Trading Classification Bot",
     "desc": "Soft trading signal classification based on Multi Layer Perceptron made in Keras.",
     "date": { "start": "20/09", "end": "20/10" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
-    "img": "/works/assets/images/soft_trade_classify_ann.png"
+    "links": { "git": "", "web": "" },
+    "img": BASE_DIR + "soft_trade_classify_ann.png"
   },
   {
     "groups": ['Django', 'Python'],
@@ -93,7 +95,7 @@ const worksInfo = [
     "title": "Automatic Answer Checker",
     "desc": "Upload test exam image and automatically retrieves the student score. Based on OpenCV computer vision framework.",
     "date": { "start": "22/07/10", "end": "still" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -101,7 +103,7 @@ const worksInfo = [
     "title": "Keras Models Converter",
     "desc": "Converts keras neural networks serial models to numpy class file to fast execute without heavy libraries needed.",
     "date": { "start": "22/07/10", "end": "still" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
+    "links": { "git": "", "web": "" },
     "img": ""
   },
   {
@@ -109,7 +111,7 @@ const worksInfo = [
     "title": "Inductance & Capacitance Meter",
     "desc": "Resonance frequency based inductometer and capacitor charging time capacitance meter based in Arduino.",
     "date": { "start": "20/09", "end": "20/10" },
-    "links": { "git": "https://www.google.com/", "web": "https://www.google.com/" },
-    "img": ""
+    "links": { "git": "", "web": "" },
+    "img": BASE_DIR + "lc_meter.png"
   }
 ];
